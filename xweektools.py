c0cc3c045db33a139ea91ee163d171c4c8ceab4d
C-ArenA <carlostata.ca@gmail.com>
2022-09-28 23:39:14 -0400

    Ahora puedo encontrar todos los docs dentro de un folder facilmente

--- xweektools.py
+++ xweektools.py
@@ -20,6 +20,7 @@ xweekcurrent["xweeknow"] = datetime.datetime.now().strftime("%d%m%y_%H%M%S")
 
 
 def main():
+    initrender()
     while True:
         initrender()
         print("Estos son los datos actuales del evento:")
@@ -99,6 +100,55 @@ def update_data(xweekconfig, xweekevent, xweekpastevent):
         multicolumn_complete=True
     ).execute()
 
+def create_event(xweekconfig, xweekevent, xweekpastevent):
+    """Ayuda al usuario a crear el evento
+    Args:
+        xweekconfig (_type_): _description_
+        xweekevent (_type_): _description_
+        xweekpastevent (_type_): _description_
+    """
+    # --- 1. Nombre del Evento
+    xweekevent["event_name"] = inquirer.text(
+        message="Nombre del Evento:",
+        default=xweekpastevent["event_name"],
+        completer={name: None for name in xweekconfig["event_names"]},
+        multicolumn_complete=True).execute()
+    # --- 2. Lugar del Evento
+    xweekevent["event_location"] = inquirer.text(
+        message="Lugar del evento:",
+        default=xweekpastevent["event_location"],
+        completer={
+            location: None for location in xweekconfig["event_locations"]},
+        multicolumn_complete=True).execute()
+    # --- 3. Abreviación del lugar del Evento
+    try:
+        default_loc_abb = xweekconfig["event_locations_abbreviations"][xweekconfig["event_locations"].index(
+            xweekevent["event_location"])]
+    except:
+        default_loc_abb = ""
+    xweekevent["event_location_abbreviation"] = inquirer.text(
+        message="Forma abreviada del lugar del evento:",
+        default=default_loc_abb,
+        completer={
+            location_abb: None for location_abb in xweekconfig["event_locations_abbreviations"]},
+        multicolumn_complete=True).execute()
+    # --- 4. Versión del Evento
+    xweekevent["event_version"] = inquirer.number(
+        message="Versión del evento (Número)",
+        validate=validator.EmptyInputValidator()
+    ).execute()
+    # --- 5. Dominio del Evento
+    try:
+        default_domain = xweekconfig["event_domains"][xweekconfig["event_names"].index(
+            xweekevent["event_name"])]
+    except:
+        default_domain = xweekpastevent["event_domain"]
+    xweekevent["event_domain"] = inquirer.text(
+        message="Dominio Web del evento:",
+          default=default_domain,
+        completer={dom:None for dom in xweekconfig["event_domains"]},
+        multicolumn_complete=True
+    ).execute()    
 
 if __name__ == "__main__":
     main()
